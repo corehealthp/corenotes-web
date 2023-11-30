8d77b8f70e99833d8eec949255c1e96999577fc5
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2023-11-30 15:58:55 +0100

    removed close modal outside click, fixed deployment error

--- src/components/Modal/ModalContainer/ModalContainer.tsx
+++ src/components/Modal/ModalContainer/ModalContainer.tsx
@@ -3,7 +3,9 @@ import styles from "./modalcontainer.module.css";
 export default function ModalContainer({ children, close, contentContainerWidth }:{ children:JSX.Element, close:()=> void, contentContainerWidth?:string }) {
     return (
         <div className={styles.modal_container}>
-            <div className={styles.modal_bg} onClick={()=> close()}></div>
+            <div className={styles.modal_bg} 
+            // onClick={()=> close()}
+            ></div>
             
             <div className={styles.content_container} style={{width:contentContainerWidth}}>
                 { children }
