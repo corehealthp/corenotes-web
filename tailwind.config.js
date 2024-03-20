0fe348318a7adebfad41d5a7ddfcd4d708d8c346
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2024-03-20 14:57:37 +0100

    nextui error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
-import {nextui}  from "@nextui-org/react";
-const { nextui } = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/***/**/*.{js,jsx,ts,tsx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 
 ],
   theme: {
     extend: {},
   },
-  plugins: [nextui()],
+  plugins: [],
 }
 
 
